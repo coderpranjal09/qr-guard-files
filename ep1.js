32b34e9432b70fe365b4d154d77fa6f85fc3363d
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2025-10-18 20:06:19 +0530

    Add files via upload

---
+++ ep1.js
@@ -0,0 +1,38 @@
+const employeeData = [
+    { 
+        name: "Pranjal Maithani",
+        mobno: "8791273578",
+        emailid: "pranjalmaithani81822@gmail.com",
+        role: "admin"
+    },{
+        name:"Santosh Bohra",
+        mobno:"8126376009",
+        emailid:"universebhaijan111@gmail.com",
+        role:"admin"
+    }
+    ,{
+        name:"Rishiraj Rawat",
+        mobno:"6397758094",
+        emailid:"rishirajrawat15@gmail.com",
+        role:"admin"
+    }
+    ,{
+        name:"Vishesh Pokhriyal",
+        mobno:"7078268582",
+        emailid:"visheshpokhriyal598@gmail.com",
+        role:"admin"
+    }
+    ,{
+        name:"Anisha Bhandari",
+        mobno:"7906026357",
+        emailid:"anishabhandari0013@gmail.com",
+        role:"admin"
+    }
+    ,{
+        name:"Radhika Bhatt",
+        mobno:"6280864435",
+        emailid:"radhikabhatt467@gmail.com",
+        role:"admin"
+    }
+    // Add more employees as needed
+];
---
+++ parking.js
@@ -0,0 +1,76 @@
+const parkingPartners = [
+  {
+      id: 1,
+      name: "THDC IHET Admin Parking",
+      coordinates: [30.38334, 78.46708],
+      slots: 50,
+      available: 30,
+      normalRate: 50,
+      qrGuardDiscount: 20,
+      operator: "QR Guard Technologies",
+      address: "Near Main Admin Building",
+      rateUnit: "per hour"
+  },
+  {
+      id: 2,
+      name: "Mall Parking Lot",
+      address: "456 Shopping Ave, Mall Road",
+      coordinates: [28.6200, 77.2150],
+      slots: 200,
+      available: 45,
+      normalRate: 120,
+      qrGuardDiscount: 90,
+      operator: "Premier Parking",
+      rateUnit: "per hour"
+  },
+  {
+      id: 3,
+      name: "Station Parking",
+      address: "Near Central Station",
+      coordinates: [28.6050, 77.2000],
+      slots: 80,
+      available: 5,
+      normalRate: 80,
+      qrGuardDiscount: 60,
+      operator: "Railway Parking",
+      rateUnit: "per hour"
+  },
+  {
+      id: 4,
+      name: "THDC IHET Hostel Parking ",
+      coordinates: [30.38472, 78.46389],
+      slots: 50,
+      available: 30,
+      normalRate: 50,
+      qrGuardDiscount: 20,
+      operator: "QR Guard Technologies",
+      address: "Near Main Hostel Building",
+      rateUnit: "per hour"
+  }
+  ,
+  
+  {
+    id: 5,
+    name: "THDC IHET F block Parking",
+    coordinates: [30.38336, 78.46450],
+    slots: 50,
+    available: 30,
+    normalRate: 50,
+    qrGuardDiscount: 20,
+    operator: "QR Guard Technologies",
+    address: "Near Main F Block Building",
+    rateUnit: "per hour"
+},
+   {
+    id: 6,
+    name: " Utu Parking",
+    coordinates: [30.343202, 77.939802],
+    slots: 50,
+    available: 30,
+    normalRate: 50,
+    qrGuardDiscount: 20,
+    operator: "QR Guard Technologies",
+    address: "Near Main Building",
+    rateUnit: "per hour"
+}
+];
