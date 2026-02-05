86aa8f20070ae2acfbf85a851c242227c7593201
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2026-02-05 22:51:51 +0530

    Refactor vehicleData array initialization

--- z01.js
+++ z01.js
@@ -1,6 +1,6 @@
 const vehicleData = [
     { vehicleID: "1234AB", model: "Toyota Corolla", vehicleNo: "UK07CAB1234", driverName: "Pranjal Maithani", ownerName: "Pranjal Maithani" },
     { vehicleID: "7891AB", model: "Hyundai i20", vehicleNo: "MN01X3927", driverName: "Lenin Sarungbam", ownerName: "Lenin Sarungbam" },
-    { vehicleID: "1223CB", model: "Mahindra Bolero", vehicleNo: "UK07GE0029", driverName: "Bipin", ownerName: "Rto Tehri" },
-     { vehicleID: "1234AE", model: "Toyota Corolla", vehicleNo: "UK07CAB6734", driverName: "Pranjal Maithani", ownerName: "Pranjal Maithani" }
+    { vehicleID: "1223CB", model: "Mahindra Bolero", vehicleNo: "UK07GE0029", driverName: "Bipin", ownerName: "Rto Tehri" }
 ];
+
