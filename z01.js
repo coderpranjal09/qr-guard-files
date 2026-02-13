f68a5990d2f9c0cbd1ae12b8f3d13a6029e10e39
Pranjal Maithani <Pranjalmaithani81822@gmail.com>
2026-02-13 18:10:42 +0530

    Update z01.js

--- z01.js
+++ z01.js
@@ -1,7 +1,8 @@
 const vehicleData = [
     { vehicleID: "1234AB", model: "Toyota Corolla", vehicleNo: "UK07CAB1234", driverName: "Pranjal Maithani", ownerName: "Pranjal Maithani" },
     { vehicleID: "7891AB", model: "Hyundai i20", vehicleNo: "MN01X3927", driverName: "Lenin Sarungbam", ownerName: "Lenin Sarungbam" },
-    { vehicleID: "1234AO", model: "Mahindra Bolero", vehicleNo: "UK07GE0029", driverName: "Bipin", ownerName: "Rto Tehri" }
+    { vehicleID: "1234AE", model: "Mahindra Bolero", vehicleNo: "UK07GE0029", driverName: "Bipin", ownerName: "Rto Tehri" }
 ];
+
 
 
